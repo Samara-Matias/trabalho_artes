8a4961a8c433d45f686d3a4ec9065031b3d594b2
unknown <caioamorimribeiro@gmail.com>
2025-05-03 16:33:42 -0300

    Atualzação LoginController

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,25 +1,33 @@
 <?php
-//classe de login
 class Login extends Controller {
     public function index() {
         $dados = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $usuario = new Usuario;
-            $email = $_POST['email'];
-            $senha = $_POST['senha'];
-            // Validação básica
-            if (empty($email) || empty($senha)) {
-                $dados['erros'] = ['Preencha email e/ou senha'];
-            } else {
-                $arr['email'] = $email;
-                $usuarioEncontrado = $usuario->first($arr);
-                if ($usuarioEncontrado && password_verify($senha, $usuarioEncontrado->senha)) { 
-                    $_SESSION['usuario'] = $usuarioEncontrado;
-                    Helper::redirecionar('dashboard');
-                    exit;
+            try {
+                $usuario = new Usuario;
+                $email = $_POST['email'];
+                $senha = $_POST['senha'];
+                // Validação básica
+                if (empty($email) || empty($senha)) {
+                    $usuario->erros = ['Preencha email e senha'];
                 } else {
-                    $dados['erros'] = ['Email/senha incorretos'];
+                    $arr['email'] = $email;
+                    $usuarioEncontrado = $usuario->first($arr);
+                    
+                    if ($usuarioEncontrado && password_verify($senha, $usuarioEncontrado->senha)) { 
+                        $_SESSION['usuario'] = $usuarioEncontrado;
+                        Helper::redirecionar('dashboard');
+                        exit;
+                    } else {
+                        $usuario->erros = ['Email/senha incorretos'];
+                    }
                 }
+                $dados['erros'] = $usuario->erros;
+                
+            } catch (PDOException $erro) {
+                error_log($erro->getMessage());
+                $usuario->erros = [];
+                $dados['erros'] = $usuario->erros;
             }
         }
         $this->view('login', $dados);
