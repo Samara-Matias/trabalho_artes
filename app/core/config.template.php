709e33c4628f21071bf7b1fa2914f3106c5444e9
unknown <caioamorimribeiro@gmail.com>
2025-05-03 16:16:07 -0300

    Atualização de pequenas partes que poderiam causar problema

--- app/core/config.template.php
+++ app/core/config.template.php
@@ -1,19 +1,8 @@
 <?php
-    if ($_SERVER['SERVER_NAME'] === 'localhost') {
-        define('DB_HOST', 'hostDoBancoDeDados');
-        define('DB_USERNAME', 'usuarioDoBancoDeDados');
-        define('DB_PASSWORD', 'senhaDoBancoDeDados');
-        define('DB_NAME', 'nomeDoBancoDeDados');
-        define('BASE_URL', 'urlBaseDoSite');
-        define('BASE_PATH', __DIR__);
-    }
-    else {
-        define('DB_HOST', '');
-        define('DB_USERNAME', '');
-        define('DB_PASSWORD', '');
-        define('DB_NAME', '');
-        define('BASE_URL', '');
-    }
+    define('DB_HOST', 'hostDoBancoDeDados');
+    define('DB_USERNAME', 'usuarioDoBancoDeDados');
+    define('DB_PASSWORD', 'senhaDoBancoDeDados');
+    define('DB_NAME', 'nomeDoBancoDeDados');
+    define('BASE_URL', 'urlBaseDoSite');
     define('BASE_PATH', __DIR__);
-    
 ?>
