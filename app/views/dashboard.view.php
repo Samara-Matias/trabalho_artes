38f1493250c16a600c74eb927979352e153c9bbb
Samara-Matias <samaramatiasdosanjos@gmail.com>
2025-05-11 20:30:37 -0300

    Criação do dashboard e customização dele

--- app/views/dashboard.view.php
+++ app/views/dashboard.view.php
@@ -15,8 +15,90 @@
     <title>Dashboard - Focus List</title>
 
     <link rel="stylesheet" href="<?= BASE_URL ?>/css/dashboard.css">
+
+    <!-- Boxicons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 </head>
 <body>
-    <h1>Bem-vindo(a) <?= $_SESSION['usuario']?></h1>
+    <nav>
+        <h2>Focus List</h2>
+        <ul>
+            <li>
+                <a href="#">Dashboard</a>
+            </li>
+            <li>
+                <a href="#">Tarefas</a>
+            </li>
+            
+        </ul>
+        <div class="user_control">
+            
+            <p class="username"><?= $_SESSION['usuario']?></p>
+            <img src="<?= BASE_URL ?>/assets/imagens/dropdownIcon.svg" alt="Chevron Down Icon BOXICON">
+            
+        </div>
+        
+        <div class="sub-menu-wrap">
+            
+            <div class="sub-menu">
+                <div class="user-info">
+                    <p class="username"><?= $_SESSION['usuario']?></p>
+                    <a href="<?= BASE_URL ?>/logout">Logout</a>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+    <main>
+
+        <header class="main__header">
+            <h1>Bem-vindo(a) <?= $_SESSION['usuario']?></h1>
+        </header>
+
+        <div class="main__container">
+
+            <div class="progresso">
+
+                <div class="progresso__card">
+                    <div class="card__header">
+                        <h3 class="card_header__title">Tarefas concluídas:</h3>
+                    </div>
+
+                    <div class="card__content">
+                        <p class="tarefas_concluidas">0/0</p>
+                    </div>
+
+                </div>
+
+                <div class="progresso__card">
+
+                    <div class="card__header">
+                        <h3 class="card_header__title">Tarefas pendentes:</h3>
+                    </div>
+
+                    <div class="card__content">
+                        <p class="tarefas_pendentes">0/0</p>
+                    </div>
+
+                </div>
+
+                <div class="progresso__card">
+
+                    <div class="card__header">
+                        <h3 class="card_header__title">Tarefas em progresso:</h3>
+                    </div>
+
+                    <div class="card__content">
+                        <p class="tarefas_em_progresso">0/0</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </main>
 </body>
 </html>
